6fac59e348ef63276c396ef98ba92bb328c05b28
wstcl <gaojunwtc@163.com>
2018-10-16 20:26:28 -0700

    RNN train

--- RNN.py
+++ RNN.py
@@ -1,17 +1,14 @@
 import pandas as pd
-import numpy as np
 import tensorflow as tf
-import tensorflow_utils
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from sklearn import model_selection
-from sklearn import metrics
-from sklearn.metrics import confusion_matrix, precision_score,recall_score,f1_score
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+from sklearn.model_selection import train_test_split
 '''
-csvname = 'C:\\Users\\93621\\Desktop\\S_dos.csv'
+csvname = 'C:\\Users\\93621\\Desktop\\L_dos.csv'
 data = pd.read_csv(csvname)
 headers = ['SourceIp','DestIp','SourcePort','destPort','Trans_Id','funcCode','Refno','Register_data','Exeption_Code','Time_Stamp']
 data.columns = headers
@@ -35,38 +32,29 @@ while(i < packets_num - 3):
         if len(data['Register_data'][i+3]) > 10:
             data['Alarm'][i:i+3]  = 1
     i = i + 1
+if i == packets_num - 3:
+    if len(data['Register_data'][i+1]) > 10:
+        data['Register_data'][i + 1] = 123
+    if len(data['Register_data'][i+2]) > 10:
+        data['Register_data'][i + 1] = 123
 print("Processing is over, start training")
+data.to_csv('C:\\Users\\93621\\Desktop\\L_LogLabeled.csv')
 '''
-csvname = 'C:\\Users\\93621\\Desktop\\LogLabeled.csv'
+csvname = 'C:\\Users\\93621\\Desktop\\L_LogLabeled.csv'
 data = pd.read_csv(csvname)
-n_classes = 2
-#data.to_csv('C:\\Users\\93621\\Desktop\\LogLabeled.csv')
+
 X = [i for i in data.columns.tolist() if i not in 'Alarm']
 X = data[X]
-X = X.as_matrix().astype(np.float64)
 y = data['Alarm']
-y = y.as_matrix().astype(np.int32)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-'''features_cols = tf.contrib.learn.infer_real_valued_columns_from_input(X_train)
-dnn_clf = tf.contrib.learn.DNNClassifier(hidden_units=[50,10,5], n_classes=2, feature_columns=features_cols)
-dnn_clf.fit(X_train, y_train, batch_size=50, steps=100)
-from sklearn.metrics import accuracy_score
-y_pred = dnn_clf.predict(X_test)
-print(accuracy_score(y_test, list(y_pred)))'''
-def logistic():
-    logreg = LogisticRegression()
-    logreg.fit(X_train,y_train)
-    y_pred = logreg.predict(X_test)
-    print(logreg.score(X_test, y_test))
-
-def NN():
-    model = Sequential()
-    model.add(Dense(10, input_dim=10,activation='relu'))
-    model.add(Dense(5,activation='relu'))
-    model.add(Dense(1,activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
-    model.fit(X_train, y_train,batch_size=100,epochs=40)
-    scores = model.evaluate(X_test,y_test)
-    print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-logistic()
+max_review_length = 10
+embedding_vecor_length = 64
+model = Sequential()
+model.add(Embedding(10000, embedding_vecor_length, input_length=max_review_length))
+model.add(LSTM(300))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+model.fit(X_train, y_train, epochs=4, batch_size=64)
+scores = model.evaluate(X_test, y_test)
+print(scores)
