521d9375405846d85a8208afc62b78dd7338cd2b
wstcl <gaojunwtc@163.com>
2018-11-06 23:10:19 -0800

    test_version

--- dostraffic.sh
+++ dostraffic.sh
@@ -9,24 +9,28 @@ do
 	if [ $R -gt 0 ]; then
 		if [ $R1 -eq 0 ]; then
 		#Incorrect CRC
-			for i in {1..100};
+			modpoll -r 52210 -0 -1 -t 4 $IP
+			for i in {1..150};
 			do
-			 modpoll -m enc -t 3 -0 -r 32210 -l 1 $IP;
+			 modpoll -m enc -t 4 -0 -1 -r 32210 -l 1 $IP;
 			done
+			modpoll -r 52211 -0 -1 -t 4  $IP
 		elif [ $R1 -eq 1 ]; then
 		#scan
+			modpoll -r 52210 -0 -1 -t 4 $IP
 			for i in {1..100};
 			do
-			 modpoll -r 42210 -c 5 -1 -l 1 $IP;
+			 modpoll -t 4 -r 42210 -0 -1 -l 1 $IP;
 			done
+			modpoll -r 52211 -0 -1 -t 4 $IP
 		fi
 	elif [ $R -eq 0 ]; then
 		if [ $R1 -eq 0 ]; then
 		#normal CRC
-			timeout 4 modpoll -m enc -t 3 -0 -r 32210 $IP;
+			timeout $(($RANDOM%2+1)) modpoll -m enc -t 4 -0 -r 32210 $IP;
 		elif [ $R1 -eq 1 ]; then
 		#normal scan
-			modpoll -r 42210 -c 10 -1 $IP;
+			timeout $(($RANDOM%2+1)) modpoll -r 42210 -t 4 -0 $IP;
 		fi
 	fi
 done
