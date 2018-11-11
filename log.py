5756d4d82558403733dac43a5d4bda944bbe7f3a
wstcl <gaojunwtc@163.com>
2018-11-11 01:40:24 -0800

    log

--- log.py
+++ log.py
@@ -1,73 +1,75 @@
 import pandas as pd
 import numpy as np
-import tensorflow as tf
-import tensorflow_utils
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from sklearn import model_selection
-from sklearn import metrics
-from sklearn.metrics import confusion_matrix, precision_score,recall_score,f1_score
 from keras.models import Sequential
 from keras.layers import Dense
-'''
-csvname = 'C:\\Users\\93621\\Desktop\\S_dos.csv'
-data = pd.read_csv(csvname)
-headers = ['SourceIp','DestIp','SourcePort','destPort','Trans_Id','funcCode','Refno','Register_data','Exeption_Code','Time_Stamp']
-data.columns = headers
-new = [ "Alarm" ]
-for i in new:
-    data[i] = np.nan
-i = 0
-data = data.fillna('0')
-packets_num = len(data['Alarm'])
-IPsource = data['SourceIp'].unique().tolist()
-IPdest = data['DestIp'].unique().tolist()
-IP = list(set(IPsource + IPdest))
-for j in range(0, len(IP)):
-    data = data.replace(IP[j],j)
-while(i < packets_num - 3):
-    if data['Exeption_Code'][i] == 1:
-        data['Alarm'][i] = 1
-    if len(data['Register_data'][i+1]) > 10:
-        data['Register_data'][i + 1] = 123
-        if len(data['Register_data'][i+3]) > 10:
-            data['Alarm'][i:i+3]  = 1
-    i = i + 1
-print("Processing is over, start training")
-'''
-csvname = 'D:\dos_pcap\OCT27_output.csv'
-data = pd.read_csv(csvname)
-def pre_split(input_data):
-    X = input_data[X_Label]
+from sklearn.metrics import precision_score,recall_score,f1_score
+
+def evaluate(y_pred, y_test):
+    pre = precision_score(y_true=y_test,y_pred=y_pred)
+    rec = recall_score(y_true=y_test,y_pred=y_pred)
+    f1 = f1_score(y_true=y_test,y_pred=y_pred)
+    return pre, rec, f1
+
+def pre_split(input_data, Label):
+    X = input_data[Label]
     X = np.array(X,dtype=float)
     y = input_data['Alarm']
     y = np.array(y,dtype=int)
     return X, y
-n_classes = 1
-#data.to_csv('C:\\Users\\93621\\Desktop\\LogLabeled.csv')
-X_Label = [i for i in data.columns.tolist() if i not in 'Alarm']
-X, y = pre_split(data)
-#X_test, y_test = pre_split(test)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+def logistic(csvname):
+    data = pd.read_csv(csvname)
+    X_Label = [i for i in data.columns.tolist() if i not in 'Alarm']
+    X, y = pre_split(data, X_Label)
+    P_score = []
+    R_score = []
+    F_score = []
+    for kfold in range(10):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        model = Sequential()
+        model.add(Dense(1, input_dim= 13, activation='sigmoid'))
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        model.fit(X_train, y_train, batch_size=50, epochs=10)
+        y_pre = model.predict_classes(X_test)
+        precision, recall, f1 = evaluate(y_pre, y_test)
+        P_score.append(precision)
+        R_score.append(recall)
+        F_score.append(f1)
+    P_score = np.array(P_score)
+    R_score = np.array(R_score)
+    F_score = np.array(F_score)
+    print("Precision: ", np.mean(P_score), "+-", np.std(P_score))
+    print("Recall: ", np.mean(R_score), "+-", np.std(R_score))
+    print("F1_socre: ", np.mean(F_score), "+-", np.std(F_score))
 
-def logistic():
-    model = Sequential()
-    model.add(Dense(1, input_dim= 11, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
-    model.fit(X_test, y_test, batch_size=100, epochs=100)
-    scores = model.evaluate(X_test, y_test)
-    print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 
+def NN(csvname):
+    data = pd.read_csv(csvname)
+    X_Label = [i for i in data.columns.tolist() if i not in 'Alarm']
+    X, y = pre_split(data, X_Label)
+    P_score = []
+    R_score = []
+    F_score = []
+    for kfold in range(10):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        model = Sequential()
+        model.add(Dense(10, input_dim=13,activation='relu'))
+        model.add(Dense(5,activation='relu'))
+        model.add(Dense(1,activation='sigmoid'))
+        model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+        model.fit(X_train, y_train,batch_size=50,epochs=50)
+        y_pre = model.predict_classes(X_test)
 
-def NN():
-    model = Sequential()
-    model.add(Dense(10, input_dim=11,activation='relu'))
-    model.add(Dense(5,activation='relu'))
-    model.add(Dense(1,activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
-    model.fit(X_test, y_test,batch_size=100,epochs=50)
-    scores = model.evaluate(X_test,y_test)
-    print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-logistic()
+        precision, recall, f1 = evaluate(y_pre, y_test)
+        P_score.append(precision)
+        R_score.append(recall)
+        F_score.append(f1)
+
+    P_score = np.array(P_score)
+    R_score = np.array(R_score)
+    F_score = np.array(F_score)
+    print("Precision: ", np.mean(P_score), "+-", np.std(P_score))
+    print("Recall: ", np.mean(R_score), "+-", np.std(R_score))
+    print("F1_socre: ", np.mean(F_score), "+-", np.std(F_score))
+NN('D:\dos_pcap\\nov8mn_output.csv')
