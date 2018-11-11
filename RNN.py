beecde58003ef21b9638c41bd7eccb148923e5d1
wstcl <gaojunwtc@163.com>
2018-11-10 19:53:52 -0800

    reshape function

--- RNN.py
+++ RNN.py
@@ -13,11 +13,18 @@ def evaluate(y_pred, y_test):
     f1 = f1_score(y_true=y_test,y_pred=y_pred)
     return pre, rec, f1
 
-def Is_stream_change(new,old):
-    if new == old:
-        return 0
-    else:
-        return 1
+def data_reshape(dst, label, timesteps):
+    input = dst[label]
+    Y = dst['Alarm']
+    input = np.array(input)
+    Y = np.array(Y)
+    drop = input.shape[0] % timesteps
+    for i in range(drop):
+        input = np.delete(input, input.shape[0] - i - 1, axis=0)
+        Y = np.delete(Y, Y.shape[0] - i - 1, axis=0)
+    X = input.reshape((input.shape[0] // timesteps, timesteps, input.shape[1]))
+    y = Y.reshape(Y.shape[0] // timesteps, timesteps)
+    return X, y
 
 def data_process(input, output):
     data = pd.read_csv(input)
@@ -27,39 +34,23 @@ def data_process(input, output):
     for i in new:
         data[i] = np.nan
     i = 0
-    stream_index = 0
+    flag = 0
     data = data.fillna('0')
     packets_num = len(data['Alarm'])
-    ukp_index = []
-    scan_index = 0
     while(i < packets_num):
-        if data['Exeption_Code'][i] == 1:
-            if data['tcp_stream_Index'][i] == stream_index:
-                data['Alarm'][index] = 0
-            else:
+        if data['SourceIp'][i] == "192.168.100.11" or data['DestIp'][i] == "192.168.100.11":
+            if data['Refno'][i] == 52210:
+                data.drop([i],inplace=True)
+                i = i + 1
+                flag = 1
+                continue
+            if data['Refno'][i] == 52211:
+                data.drop([i],inplace=True)
+                i = i + 1
+                flag = 0
+                continue
+            if data['Refno'][i] != 52210 and data['Refno'][i] != 52211 and flag == 1:
                 data['Alarm'][i] = 1
-                stream_index = data['tcp_stream_Index'][i]
-                index = i
-
-        if data['funcCode'][i] == 3:
-            if data['SourceIp'][i] == "192.168.100.11" or data['DestIp'][i] == "192.168.100.11":    # 192.168.100.11 could be replaced by a out net e.g. if data not in [inner_net]
-                if data['Trans_Id'][i] == 1:
-                    if data['tcp_stream_Index'][i] != scan_index:
-                        scan_index = data['tcp_stream_Index'][i]
-                        data['Alarm'][i] = 1
-                        ukp_index.append(i)
-                    if data['tcp_stream_Index'][i] == scan_index and data['DestIp'][i] == '192.168.100.11':
-                        data['Alarm'][i] = 1
-                        ukp_index.append(i)
-                if data['Trans_Id'][i] == 2:
-                    if len(ukp_index) > 1:
-                        data['Alarm'][ukp_index[-1],ukp_index[-2]] = 0
-                        ukp_index = []
-                    if len(ukp_index) == 1:
-                        data['Alarm'][ukp_index[-1]] = 0
-                        ukp_index = []
-
-
         i = i + 1
 
     IPsource = data['SourceIp'].unique().tolist()
@@ -71,44 +62,28 @@ def data_process(input, output):
     print("Processing is over, start training")
 
 
-
 def RNN(csvname):
     data = pd.read_csv(csvname)
-    #test = pd.read_csv('D:\dos_pcap\OCT30MN_output.csv')
+    test = pd.read_csv('D:\\dos_pcap\\nov6_output.csv')
+    timestep = 5
     X_Label = [i for i in data.columns.tolist() if i not in 'Alarm']
-    X = data[X_Label]
-    y = data['Alarm']
-    X = np.array(X)
-    X = X.reshape((X.shape[0], 1, X.shape[1]))
-    P_score = []
-    R_score = []
-    F_score = []
-    '''test_x = test[X_Label]
-    test_y = test['Alarm']
-    test_x = np.array(test_x)
-    test_x = test_x.reshape((test_x.shape[0], 1, test_x.shape[1]))'''
+    X, y = data_reshape(data, X_Label, timestep)
+    test_x, test_y = data_reshape(test, X_Label, timestep)
 
-    for kfold in range(5):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=40)
+    for kfold in range(1):
         model = Sequential()
-        model.add(LSTM(256, return_sequences=True,input_shape=(X_train.shape[1],X_train.shape[2])))
+        model.add(LSTM(256, return_sequences=True,input_shape=(X.shape[1],X.shape[2])))
         model.add(LSTM(128))
-        model.add(Dense(1, activation='sigmoid'))
+        model.add(Dense(timestep, activation='sigmoid'))
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
         print(model.summary())
-        model.fit(X_train, y_train, epochs=15, batch_size=500)
-        y_pre = model.predict_classes(X_test)
-        model.save('D:\SCADA_PROJECT\my_model.h5')
-        precision, recall, f1 = evaluate(y_pre,y_test)
-        P_score.append(precision)
-        R_score.append(recall)
-        F_score.append(f1)
-    P_score = np.array(P_score)
-    R_score = np.array(R_score)
-    F_score = np.array(F_score)
-    print("Precision: ", np.mean(P_score),"+-" , np.std(P_score))
-    print("Recall: ", np.mean(R_score), "+-", np.std(R_score))
-    print("F1_socre: ", np.mean(F_score), "+-", np.std(F_score))
+        model.fit(X, y, epochs=5, batch_size=128)
+        score = model.evaluate(test_x,test_y)
+        print(score)
+    print("Done")
 
-#data_process('D:\\dos_pcap\\nov6.csv','D:\\dos_pcap\\nov6_output.csv')
-RNN('D:\\dos_pcap\\OCT30MN_output.csv')
+
+
+data_process('D:\\dos_pcap\\nov8L.csv','D:\\dos_pcap\\nov8L_output.csv')
+#data_process('D:\\dos_pcap\\nov8mn.csv','D:\\dos_pcap\\nov8mn_output.csv')
+RNN('D:\\dos_pcap\\nov8L_output.csv')
