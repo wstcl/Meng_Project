5d007b34031b1577b835b7415c92b0bdea2ceed6
wstcl <gaojunwtc@163.com>
2019-01-09 18:07:34 -0800

    Tested version

--- Modbus_IDS.py
+++ Modbus_IDS.py
@@ -3,14 +3,13 @@ import numpy as np
 from keras.models import load_model
 import h5py
 import time
-feature_mean = []
-feature_std = []
+feature_mean = [987077.1708173917, 2134742.0481606494, 17459.159539713866, 26849.103009466908, 37612.68168409985, 9418.126090530171, 5.948326289130788, 19811.18698915624, 19.18415200457282, 0.20418730207737915, 305.4710627127993, 0.013863061511296453]
+feature_std = [1837141.711676915, 2624416.776463739, 21106.398363980843, 21910.772814885655, 45740.66561929073, 11063.244155796494, 6.279870133414408, 18855.33330669925, 39.37499182205279, 0.4031073377214498, 367.18606771190304, 0.032363656194118]
 feature_mean = np.array(feature_mean)
 feature_std = np.array(feature_std)
 i = 0
 model = load_model('lstm.h5')
 capture = pk.LiveCapture('br0', display_filter = 'mbtcp')
-capture.set_debug()
 print('Modbus NIDS start, Press CTRL+C to stop')
 while (True):
     for packet in capture.sniff_continuously():
@@ -55,18 +54,14 @@ while (True):
             i = 0
             input = (raw -feature_mean)/feature_std
             input = input.reshape((1,5,12))
-            label = model.predict(input)
-            output = np.insert(raw,13,label,axis=1)
-            if label.any() > 0.5:
+            label = model.predict(input) 
+            label[label>0.5]=1
+            label[label<=0.5]=0
+            output = np.insert(raw,12,label,axis=1)
+            if label.any() == 1:
                 print(time.ctime(),"Dos attack detected")
+                print(label)
             f=open('Log.csv','ab')
             np.savetxt(f,output,delimiter=",")
             f.close()
-
-
-
-
-
-
-        
 
