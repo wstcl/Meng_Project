a6fc16935fc43d8b52509d024c9ea32d710e9e94
wstcl <gaojunwtc@163.com>
2019-04-03 00:50:50 -0700

    10fold learning curve

--- RNN.py
+++ RNN.py
@@ -5,9 +5,11 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 import keras
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 #import h5py
-
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
 IPhash = {'10.0.0.3':297913,
           '10.0.0.4':297914,
           '10.0.0.5':297915,
@@ -55,6 +57,50 @@ def classreport(y_true, y_pred):
     f1_score = 2 * (precision * recall) / (precision + recall)
     return precision,recall,f1_score
 
+def data_reshape_multilabel(dst, label, timesteps,feature_mean=[], feature_std=[],model=0):
+    if feature_mean != []:
+        mean = feature_mean
+        std = feature_std
+        index = 0
+        for i in label:
+            mean = feature_mean[index]
+            std = feature_std[index]
+            dst[i] = (dst[i] - mean) / std
+            index = index + 1
+        print("feature_mean:", feature_mean)
+        print("feature_std:", feature_std)
+    if feature_mean == []:
+        #Standardization
+        feature_mean = []
+        feature_std = []
+        for i in label:
+            mean = dst[i].mean()
+            std = dst[i].std()
+            feature_mean.append(mean)
+            feature_std.append(std)
+            dst[i] = (dst[i] - mean) / std
+        print("feature_mean:", feature_mean)
+        print("feature_std:", feature_std)
+
+    #reshape
+    input = dst[label]
+    Y = dst['Alarm']
+    input = np.array(input)
+    Y = np.array(Y)
+    Y = to_categorical(Y)
+    print(Y)
+    drop = input.shape[0] % timesteps
+    for i in range(drop):
+        input = np.delete(input, input.shape[0] - i - 1, axis=0)
+        Y = np.delete(Y, Y.shape[0] - i - 1, axis=0)
+    X = input.reshape((input.shape[0]//timesteps, timesteps, input.shape[1]))
+    if model == 0:    #many-one
+        y = Y.reshape(Y.shape[0] // timesteps, timesteps*3)
+    if model == 1:    #many-many
+        y = Y.reshape(Y.shape[0]//timesteps,timestep,3)
+    return X, y,feature_mean,feature_std
+
+
 def data_reshape(dst, label, timesteps,feature_mean=[], feature_std=[]):
     if feature_mean != []:
         mean = feature_mean
@@ -93,126 +139,140 @@ def data_reshape(dst, label, timesteps,feature_mean=[], feature_std=[]):
     y = Y.reshape(Y.shape[0] // timesteps, timesteps)
     return X, y,feature_mean,feature_std
 
-def data_process(input, output):
-    data = pd.read_csv(input)
-    headers = ['SourceIp','DestIp','SourcePort','destPort','Seq_num','Trans_Id','funcCode','Refno','Register_data','Exeption_Code','Time_Stamp','Relative_Time','Alarm']
-    data.columns = headers
-    i = 0
-    flag = 0
-    data = data.fillna('0')
-    packets_num = len(data['Alarm'])
-    # IPhash
-    data = data.convert_objects(convert_numeric=True)
-    while(i < packets_num):
-        if data['SourceIp'][i] == 5884431 or data['DestIp'][i] == 5884431:
-            if data['Refno'][i] == 52210:
-                data.drop([i],inplace=True)
-                i = i + 1
-                data.drop([i],inplace=True)
-                i = i + 1
-                flag = 1
-                continue
-            if data['Refno'][i] == 52211:
-                data.drop([i],inplace=True)
-                i = i + 1
-                data.drop([i],inplace=True)
-                i = i + 1
-                flag = 0
-                continue
-            if data['Refno'][i] != 52210 and data['Refno'][i] != 52211 and flag == 1:
-                data['Alarm'][i] = 1
-                i = i + 1
-
-    data = data.convert_objects(convert_numeric=True)
-    #Standardization
-
-    data.to_csv(output, index=False)
-    print("Processing is over, start training")
-
-
-def RNN(csvname,testname):
+def RNN(csvname):
     data = pd.read_csv(csvname)
     headers = ['SourceIp','DestIp','SourcePort','destPort','Seq_num','Trans_Id','funcCode','Refno','Register_data','Exeption_Code','Time_Stamp','Relative_Time','Alarm']
     data.columns = headers
-    #del(data['tcp_stream_Index'])
-    #del(data['Seq_num'])
-    test = pd.read_csv(testname)
-    test.columns = headers
-    #del(test['tcp_stream_Index'])
-    #del(test['Seq_num'])
+    #del(data['eth_src'])
+    #del(data['eth_dst'])
+    #test = pd.read_csv(testname)
+    #test.columns = headers
+    #del(test['eth_src'])
+    #del(test['eth_dst'])
     timestep = 10
     X_Label = [i for i in data.columns.tolist() if i not in 'Alarm']
     X, y,features_mean,features_std = data_reshape(data, X_Label, timestep)
-    test_x, test_y,features_mean,features_std = data_reshape(test, X_Label, timestep,features_mean,features_std)
-    precision = []
-    recall = []
-    f1 = []
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.1)
+    #test_x, test_y,features_mean,features_std = data_reshape(test, X_Label, timestep,features_mean,features_std)
     num_seq = X.shape[0]
-    num_sample = [1,10,100,1000,5000,10000,15000,20000,num_seq]
+    num_sample = [10,100,500,1000,3000,6000,10000,13000,16000,20000,num_seq]
     Train_Error = []
     Test_Error = []
-    es = keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.000009, patience=5)
-    for kfold in range(1):
-        for sample_size in num_sample:
+    neurons = [2,1,4,2,8,4,16,8,32,16,64,32,128,64,256,128,512,256]
+    es = keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.00009, patience=5)
+    for sample_size in num_sample:
+        print(sample_size,file=open("10fold/LSTM_10_fold_train.txt","a"))
+        print("Precision",',',"Recall",',',"F1",',',"Accuray",',',"loss",file=open("10fold/LSTM_10_fold_train.txt","a"))
+        print(sample_size,file=open("10fold/LSTM_10_fold_test.txt","a"))
+        print("Precision",',',"Recall",',',"F1",',',"Accuray",',',"loss",file=open("10fold/LSTM_10_fold_test.txt","a"))
+        
+        for kfold in range(10):
             model = Sequential()
             model.add(LSTM(256,return_sequences=True,input_shape=(X.shape[1],X.shape[2])))
             model.add(LSTM(128))
             model.add(Dense(timestep, activation='sigmoid'))
             model.compile(loss='binary_crossentropy', optimizer='adam',metrics = ['accuracy'])
             print(model.summary())
-            model.fit(X[0:sample_size,:,:], y[0:sample_size,:], epochs=10000,shuffle = False,callbacks=[es])
-            Train_loss = model.evaluate(X[0:sample_size,:,:], y[0:sample_size,:])
-            Validation_loss =  model.evaluate(test_x,test_y)
-            Train_Error.append(Train_loss[0])
-            Test_Error.append(Validation_loss[0])
+            model.fit(X_train[0:sample_size,:,:], y_train[0:sample_size,:], epochs=5000,shuffle = True,callbacks=[es])
+            y_pre_train = model.predict(X_train[0:sample_size,:,:])
+            y_pre_test = model.predict(X_test)
+            y_true_train = y_train[0:sample_size,:]
+            ptrain,rtrain,ftrain = classreport(y_pred=y_pre_train,y_true=y_true_train)
+            ptest,rtest,ftest = classreport(y_pred=y_pre_test,y_true=y_test)
+            atest = model.evaluate(X_test,y_test)
+            atrain = model.evaluate(X_train[0:sample_size,:,:], y_train[0:sample_size,:])
+            acc_train = atrain[1]
+            acc_test = atest[1]
+            loss_train = atrain[0]
+            loss_test = atest[0]
+            print(ptrain,',',rtrain,',',ftrain,',',acc_train,',',loss_train,file=open("10fold/LSTM_10_fold_train.txt","a"))
+            print(ptest,',',rtest,',',ftest,',',acc_test,',',loss_test,file=open("10fold/LSTM_10_fold_test.txt","a"))
+             #Train_Error.append(ftrain)
+             #Test_Error.append(ftest)
+        #print(Train_Error,file=open("RNN_Neuron_Train.txt","a"))
+        #print(Test_Error,file=open("RNN_Neuron_Test.txt","a"))
+        '''
         plt.figure()
-        plt.plot(np.log10(num_sample),Train_Error)
-        plt.plot(np.log10(num_sample), Test_Error)
-        plt.xlabel('Size of training/log10')
-        plt.ylabel('Loss')
+        plt.plot(np.array([2,4,8,16,32,64,128,256,512,1024]),Train_Error)
+        plt.plot(np.array([2,4,8,16,32,64,128,256,512,1024]), Test_Error)
+        plt.xlabel('Neurons')
+        plt.ylabel('F1_score')
         plt.legend(['J$_{train}$','J$_{cv}$'],loc='upper right')
-        plt.savefig('C:\\Users\\93621\\Desktop\\SCADA plot\\RNN_learning_log.png')
+        plt.savefig('RNN_neurons_f1.png')'''
 
-
-    precision = np.array(precision)
-    recall = np.array(recall)
-    f1 = np.array(f1)
-    print("Precision：",np.mean(precision)," +- ", np.std(precision))
-    print("recall：",np.mean(recall)," +- ", np.std(recall))
-    print("f1：",np.mean(f1)," +- ", np.std(f1))
     print("Done")
-
 def Performance_evaluation(onlinecsv,output):
+    exter_eth = int('000c29d7eaec',16)
+    label_index = 14
     data = pd.read_csv(onlinecsv)
     data = np.array(data)
     y_true = data.copy()
-    y_true[:,12] = 0
+    y_true[:,label_index] = 0
     start_index = np.argwhere(data[:,7]==52210)
     end_index = np.argwhere(data[:,7]==52211)
     start_index = np.concatenate(start_index)
     end_index = np.concatenate(end_index)
     Flag_Diff = len(start_index)-len(end_index)
+    exter_index = np.argwhere(data[:,12:label_index]==exter_eth)
+    mtim_index = np.argwhere(data[exter_index[:,0],0:2]==297913)
+    mtim_index = exter_index[mtim_index[:,0],0]
+    #mtim_index = np.concatenate(mtim_index)
+    y_true[mtim_index,label_index] = 1
     if Flag_Diff==0:
         for i in range(len(start_index)):
             A_index = np.argwhere(data[start_index[i]:end_index[i],0:2]==5884431)+start_index[i]
-            y_true[A_index[:,0],12]=1
+            y_true[A_index[:,0],label_index]=1
     elif Flag_Diff == 1:
         for i in range(len(start_index)-1):
             A_index = np.argwhere(data[start_index[i]:end_index[i],0:2]==5884431)+start_index[i]
-            y_true[A_index[:,0],12]=1
+            y_true[A_index[:,0],label_index]=1
         A_index = np.argwhere(data[start_index[-1]:len(data),0:2]==5884431)+start_index[i]
-        y_true[A_index[:,0],12]=1
+        y_true[A_index[:,0],label_index]=1
     else:
          raise ValueError('Difference between start and end flag is greater than one.')
     flags=np.append(start_index,end_index)
     y_true = np.delete(y_true,flags,axis = 0)
     data = np.delete(data,flags,axis=0)
-    print(classreport(y_true[:,12],data[:,12]))
+    print(classreport(y_true[:,label_index],data[:,label_index]))
+    np.savetxt(output,y_true,delimiter=',')
+
+def Performance_evaluation_multilabel(onlinecsv,output):
+    exter_eth = int('000c29d7eaec',16)
+    label_index = 14
+    data = pd.read_csv(onlinecsv)
+    data = np.array(data)
+    y_true = data.copy()
+    y_true[:,label_index] = 0
+    start_index = np.argwhere(data[:,7]==52210)
+    end_index = np.argwhere(data[:,7]==52211)
+    start_index = np.concatenate(start_index)
+    end_index = np.concatenate(end_index)
+    Flag_Diff = len(start_index)-len(end_index)
+    exter_index = np.argwhere(data[:,12:label_index]==exter_eth)
+    mtim_index = np.argwhere(data[exter_index[:,0],0:2]==297913)
+    mtim_index = exter_index[mtim_index[:,0],0]
+    #mtim_index = np.concatenate(mtim_index)
+    y_true[mtim_index,label_index] = 2
+    if Flag_Diff==0:
+        for i in range(len(start_index)):
+            A_index = np.argwhere(data[start_index[i]:end_index[i],0:2]==5884431)+start_index[i]
+            y_true[A_index[:,0],label_index]=1
+    elif Flag_Diff == 1:
+        for i in range(len(start_index)-1):
+            A_index = np.argwhere(data[start_index[i]:end_index[i],0:2]==5884431)+start_index[i]
+            y_true[A_index[:,0],label_index]=1
+        A_index = np.argwhere(data[start_index[-1]:len(data),0:2]==5884431)+start_index[i]
+        y_true[A_index[:,0],label_index]=1
+    else:
+         raise ValueError('Difference between start and end flag is greater than one.')
+    flags=np.append(start_index,end_index)
+    y_true = np.delete(y_true,flags,axis = 0)
+    data = np.delete(data,flags,axis=0)
+    print(classreport(y_true[:,label_index],data[:,label_index]))
     np.savetxt(output,y_true,delimiter=',')
 
 
-
-
 #data_process('D:\\dos_pcap\\Dec2_4.csv','D:\\dos_pcap\\Dec2_4_output.csv')
-#Performance_evaluation('Test_Log.csv','Label_TL.csv')
-RNN('D:\\dos_pcap\\Label_Jan27b.csv','D:\\dos_pcap\\Label_Jan27a.csv')
+#Performance_evaluation('mtim.csv','Label_Mtim.csv')
+RNN('Label_Jan27b.csv')
+#Performance_evaluation_multilabel('mtim.csv','Mlabel_Mtim.csv')
--- log.py
+++ log.py
@@ -1,16 +1,27 @@
 import pandas as pd
 import numpy as np
+import keras
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense
-from sklearn.metrics import precision_score,recall_score,f1_score,confusion_matrix
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
 import matplotlib.pyplot as plt
+from keras.callbacks import EarlyStopping
 def evaluate(y_pred, y_test):
     pre = precision_score(y_true=y_test,y_pred=y_pred)
     rec = recall_score(y_true=y_test,y_pred=y_pred)
     f1 = f1_score(y_true=y_test,y_pred=y_pred)
     return pre, rec, f1
 
+class LossHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.val_losses = []
+
+    def on_batch_end(self,batch,logs={}):
+        self.losses.append(logs.get('loss'))
+        self.val_loss = self.model.evaluate(self.validation_data[0],self.validation_data[1])
+        self.val_losses.append(self.val_loss[0])
 
 def pre_split(input_data, Label):
     X = input_data[Label]
@@ -36,34 +47,45 @@ def logistic(csvname):
     data.columns = headers
     X_Label = [i for i in data.columns.tolist() if i not in 'Alarm']
     X, y = pre_split(data, X_Label)
-    P_score = []
-    R_score = []
-    F_score = []
-    for kfold in range(1):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-        model = Sequential()
-        model.add(Dense(1, input_dim= 12, activation='sigmoid'))
-        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-        history = model.fit(X_train, y_train, batch_size=1000, epochs=130,validation_split=0.2)
-        plt.plot(history.history['acc'])
-        plt.plot(history.history['val_acc'])   
-        plt.title('Logistic accuracy')     
-        plt.ylabel('accuracy')
-        plt.xlabel('epoch')
-        plt.legend(['train','test'],loc='upper left')
-        plt.show()
-        y_pre = model.predict_classes(X_test)
-        precision, recall, f1 = evaluate(y_pre, y_test)
-        P_score.append(precision)
-        R_score.append(recall)
-        F_score.append(f1)
-        print(confusion_matrix(y_test,y_pre))
-    P_score = np.array(P_score)
-    R_score = np.array(R_score)
-    F_score = np.array(F_score)
-    print("Precision: ", np.mean(P_score), "+-", np.std(P_score))
-    print("Recall: ", np.mean(R_score), "+-", np.std(R_score))
-    print("F1_socre: ", np.mean(F_score), "+-", np.std(F_score))
+    Train_Error = []
+    Test_Error = []
+    es = EarlyStopping(monitor='loss',min_delta = 0.00005, patience = 5)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+    num_seq = X_train.shape[0]
+    num_sample = [10, 100, 500, 1000, 5000, 10000, 30000, 50000, 80000, 100000, 150000, 200000, num_seq]
+    
+    for sample_size in num_sample:
+        print(sample_size,file=open("10fold/Log_10_fold_train.txt","a"))
+        print("Precision",',',"Recall",',',"F1",',',"Accuray",',',"loss",file=open("10fold/Log_10_fold_train.txt","a"))
+        print(sample_size,file=open("10fold/Log_10_fold_test.txt","a"))
+        print("Precision",',',"Recall",',',"F1",',',"Accuray",',',"loss",file=open("10fold/Log_10_fold_test.txt","a"))
+        
+        for kfold in range(10):
+            model = Sequential()
+            model.reset_states()
+            model.add(Dense(1,input_dim= 12,activation='sigmoid'))
+            model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+            model.fit(X_train[0:sample_size, :], y_train[0:sample_size], batch_size = 256, epochs=10000, shuffle=True,callbacks=[es])
+            y_pre_train = model.predict_classes(X_train[0:sample_size, :])
+            y_pre_test = model.predict_classes(X_test)
+            y_pre_train = y_pre_train.reshape(sample_size)
+            ftrain = f1_score(y_true=y_train[0:sample_size],y_pred=y_pre_train)
+            ptrain = precision_score(y_true=y_train[0:sample_size],y_pred=y_pre_train)
+            rtrain = recall_score(y_true=y_train[0:sample_size],y_pred=y_pre_train)
+            
+            ftest = f1_score(y_true=y_test,y_pred=y_pre_test)
+            ptest = precision_score(y_true=y_test,y_pred=y_pre_test)
+            rtest = recall_score(y_true=y_test,y_pred=y_pre_test)
+
+            atrain = model.evaluate(X_train[0:sample_size, :], y_train[0:sample_size])
+            atest = model.evaluate(X_test,y_test)
+            acc_train = atrain[1]
+            acc_test = atest[1]
+            loss_train = atrain[0]
+            loss_test = atest[0]
+            print(ptrain,',',rtrain,',',ftrain,',',acc_train,',',loss_train,file=open("10fold/Log_10_fold_train.txt","a"))
+            print(ptest,',',rtest,',',ftest,',',acc_test,',',loss_test,file=open("10fold/Log_10_fold_test.txt","a"))
+            
 
 
 def NN(csvname):
@@ -72,35 +94,46 @@ def NN(csvname):
     data.columns = headers
     X_Label = [i for i in data.columns.tolist() if i not in 'Alarm']
     X, y = pre_split(data, X_Label)
-    P_score = []
-    R_score = []
-    F_score = []
-    for kfold in range(1):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-        model = Sequential()
-        model.add(Dense(256, input_dim=12,activation='tanh'))
-        model.add(Dense(128,activation='tanh'))
-        model.add(Dense(1,activation='sigmoid'))
-        model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
-        history = model.fit(X_train, y_train, batch_size=1000, epochs=250,validation_split=0.2)
-        plt.plot(history.history['acc'])
-        plt.plot(history.history['val_acc'])   
-        plt.title('MLP accuracy')     
-        plt.ylabel('accuracy')
-        plt.xlabel('epoch')
-        plt.legend(['train','test'],loc='upper left')
-        plt.show()
-        y_pre = model.predict_classes(X_test)
-        precision, recall, f1 = evaluate(y_pre, y_test)
-        P_score.append(precision)
-        R_score.append(recall)
-        F_score.append(f1)
-        print(confusion_matrix(y_test,y_pre))
+    Train_Error = []
+    Test_Error = []
+    es = EarlyStopping(monitor='loss',min_delta = 0.00005, patience = 5)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+    num_seq = X_train.shape[0]
+    num_sample = [10, 100, 500, 1000, 5000, 10000, 30000, 50000, 80000, 100000, 150000, 200000, num_seq]
+    
+    for sample_size in num_sample:
+        print(sample_size,file=open("10fold/NN_10_fold_train.txt","a"))
+        print("Precision",',',"Recall",',',"F1",',',"Accuray",',',"loss",file=open("10fold/NN_10_fold_train.txt","a"))
+        print(sample_size,file=open("10fold/NN_10_fold_test.txt","a"))
+        print("Precision",',',"Recall",',',"F1",',',"Accuray",',',"loss",file=open("10fold/NN_10_fold_test.txt","a"))
+        
+        for kfold in range(10):
+            model = Sequential()
+            model.reset_states()
+            model.add(Dense(256, input_dim=12,activation='tanh'))
+            model.add(Dense(128,activation='tanh'))
+            model.add(Dense(1,input_dim= 12,activation='sigmoid'))
+            model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+            model.fit(X_train[0:sample_size, :], y_train[0:sample_size], batch_size=256, epochs=10000, shuffle=True,callbacks=[es])
+            y_pre_train = model.predict_classes(X_train[0:sample_size, :])
+            y_pre_test = model.predict_classes(X_test)
+            y_pre_train = y_pre_train.reshape(sample_size)
 
-    P_score = np.array(P_score)
-    R_score = np.array(R_score)
-    F_score = np.array(F_score)
-    print("Precision: ", np.mean(P_score), "+-", np.std(P_score))
-    print("Recall: ", np.mean(R_score), "+-", np.std(R_score))
-    print("F1_socre: ", np.mean(F_score), "+-", np.std(F_score))
+            ftrain = f1_score(y_true=y_train[0:sample_size],y_pred=y_pre_train)
+            ptrain = precision_score(y_true=y_train[0:sample_size],y_pred=y_pre_train)
+            rtrain = recall_score(y_true=y_train[0:sample_size],y_pred=y_pre_train)
+            
+            ftest = f1_score(y_true=y_test,y_pred=y_pre_test)
+            ptest = precision_score(y_true=y_test,y_pred=y_pre_test)
+            rtest = recall_score(y_true=y_test,y_pred=y_pre_test)
+
+            atrain = model.evaluate(X_train[0:sample_size, :], y_train[0:sample_size])
+            atest = model.evaluate(X_test,y_test)
+            acc_train = atrain[1]
+            acc_test = atest[1]
+            loss_train = atrain[0]
+            loss_test = atest[0]
+    
+
+logistic('Label_Jan27b.csv')
 NN('Label_Jan27b.csv')
