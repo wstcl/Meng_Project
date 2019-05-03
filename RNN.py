927ddbb016264d38b08b10d96475d5e3d9a26b81
wstcl <gaojunwtc@163.com>
2019-05-02 17:11:56 -0700

    RNN and log before new LSTM implemented

---
+++ RNN.py
@@ -0,0 +1,306 @@
+import pandas as pd
+import numpy as np
+import keras.backend as K
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+import keras
+from keras.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
+import matplotlib.pyplot as plt
+import random
+import time
+import os
+import h5py
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+IPhash = {'10.0.0.3':297913,
+          '10.0.0.4':297914,
+          '10.0.0.5':297915,
+          '192.168.100.11':5884431}
+
+
+def evaluate(y_pred, y_test):
+    pre = precision_score(y_true=y_test,y_pred=y_pred)
+    rec = recall_score(y_true=y_test,y_pred=y_pred)
+    f1 = f1_score(y_true=y_test,y_pred=y_pred)
+    return pre, rec, f1
+
+class LossHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.val_losses = []
+
+    def on_batch_end(self,batch,logs={}):
+        self.losses.append(logs.get('loss'))
+        self.val_loss = self.model.evaluate(self.validation_data[0],self.validation_data[1])
+        self.val_losses.append(self.val_loss[0])
+
+
+
+def data_reshape_multilabel(dst, label, timesteps,feature_mean=[], feature_std=[],model=1):
+    if feature_mean != []:
+        mean = feature_mean
+        std = feature_std
+        index = 0
+        for i in label:
+            mean = feature_mean[index]
+            std = feature_std[index]
+            dst[i] = (dst[i] - mean) / std
+            index = index + 1
+        print("feature_mean:", feature_mean)
+        print("feature_std:", feature_std)
+    if feature_mean == []:
+        #Standardization
+        feature_mean = []
+        feature_std = []
+        for i in label:
+            mean = dst[i].mean()
+            std = dst[i].std()
+            if std == 0:
+                std=1
+            feature_mean.append(mean)
+            feature_std.append(std)
+            dst[i] = (dst[i] - mean) / std
+        print("feature_mean:", feature_mean)
+        print("feature_std:", feature_std)
+
+    #reshape
+    input = dst[label]
+    Y = dst['Alarm']
+    input = np.array(input)
+    Y = np.array(Y)
+    Y = to_categorical(Y)
+    print(Y)
+    drop = input.shape[0] % timesteps
+    for i in range(drop):
+        input = np.delete(input, input.shape[0] - i - 1, axis=0)
+        Y = np.delete(Y, Y.shape[0] - i - 1, axis=0)
+    X = input.reshape((input.shape[0]//timesteps, timesteps, input.shape[1]))
+    if model == 0:    #many-one
+        y = Y.reshape(Y.shape[0] // timesteps, timesteps*3)
+    if model == 1:    #many-many
+        y = Y.reshape(Y.shape[0]//timesteps,timesteps,2)
+    return X, y,feature_mean,feature_std
+
+def pre_split(X,y,test_size):
+
+    print(X.shape)
+    num_packets = X.shape[0]
+
+    y = np.array(y,dtype=int)
+    #y = to_categorical(y)
+    test_indx =np.array(random.sample(range(num_packets),int(test_size*num_packets)))
+
+    train_indx = np.setdiff1d(np.array(range(num_packets)),test_indx)
+    X_train = X[train_indx]
+    y_train = y[train_indx]
+    X_test = X[test_indx]
+    y_test = y[test_indx]
+    return X_train, y_train, X_test,y_test,train_indx,test_indx
+
+def data_reshape(dst, label, timesteps,feature_mean=[], feature_std=[]):
+    if feature_mean != []:
+        mean = feature_mean
+        std = feature_std
+        index = 0
+        for i in label:
+            mean = feature_mean[index]
+            std = feature_std[index]
+
+            dst[i] = (dst[i] - mean) / std
+            index = index + 1
+        print("feature_mean:", feature_mean)
+        print("feature_std:", feature_std)
+    if feature_mean == []:
+        #Standardization
+        feature_mean = []
+        feature_std = []
+        for i in label:
+            mean = dst[i].mean()
+            std = dst[i].std()
+            if std == 0:
+                std=1
+            feature_mean.append(mean)
+            feature_std.append(std)
+            dst[i] = (dst[i] - mean) / std
+        print("feature_mean:", feature_mean)
+        print("feature_std:", feature_std)
+
+    #reshape
+    input = dst[label]
+    Y = dst['Alarm']
+    input = np.array(input)
+    Y = np.array(Y)
+    drop = input.shape[0] % timesteps
+    for i in range(drop):
+        input = np.delete(input, input.shape[0] - i - 1, axis=0)
+        Y = np.delete(Y, Y.shape[0] - i - 1, axis=0)
+    X = input.reshape((input.shape[0]//timesteps, timesteps, input.shape[1]))
+    y = Y.reshape(Y.shape[0] // timesteps, timesteps)
+    return X, y,feature_mean,feature_std
+
+def RNN(csvname):
+    data = pd.read_csv(csvname)
+    data = data.dropna()
+    #headers = ['SourceIp', 'DestIp', 'SourcePort', 'destPort', 'Seq_num', 'Trans_Id', 'funcCode', 'Refno',
+               #'Register_data', 'Exeption_Code', 'Time_Stamp', 'Relative_Time', 'Alarm']
+
+    headers = ['SourceIp','DestIp','SourcePort','destPort','Seq_num','Trans_Id','funcCode','Refno','Register_data','Exeption_Code','Time_Stamp','Relative_Time','HH','LL','H','L','speed','t1','t2','Alarm']
+    data.columns = headers
+    tm = time.ctime() + '/'
+    path = 'RNN_results/'
+    os.mkdir(path + tm)
+    os.mkdir(path + tm + 'model/')
+    train = path + tm + "train.csv"
+    test = path + tm + "test.csv"
+
+    indices_test = path + tm + "indices_test.csv"
+    
+    model_path = path + tm + 'model/'
+    #del(data['eth_src'])
+    #del(data['eth_dst'])
+
+    timestep = 10
+    X_Label = [i for i in data.columns.tolist() if i not in 'Alarm']
+    X, y,features_mean,features_std = data_reshape(data, X_Label, timestep)
+    X_train, y_train, X_test, y_test, train_indx, test_indx=pre_split(X,y,test_size=0.3)
+    #test_x, test_y,features_mean,features_std = data_reshape(test, X_Label, timestep,features_mean,features_std)
+    num_seq = X_train.shape[0]
+    #num_sample = [22,50,100,300,500,800,1000,2000,3000,5000,8000,10000,13000,14000,15000,17000,num_seq]
+    num_sample = [num_seq]
+    neurons = [2,1,4,2,8,4,16,8,32,16,64,32,128,64,256,128,512,256]
+    for sample_size in num_sample:
+        es = EarlyStopping(monitor='loss',min_delta=1e-6, patience = 5)
+        print(sample_size,file=open(train,"a"))
+        print("Precision",',',"Recall",',',"F1",',',"Accuray",',',"loss",file=open(train,"a"))
+        print(sample_size,file=open(test,"a"))
+        print("Precision",',',"Recall",',',"F1",',',"Accuray",',',"loss",file=open(test,"a"))
+        
+        
+        for kfold in range(1):
+            model = Sequential()
+            indx = np.array(random.sample(range(X_train.shape[0]), sample_size))
+            model.add(LSTM(64,return_sequences=True,input_shape=(X.shape[1],X.shape[2])))
+            model.add(LSTM(32))
+
+            model.add(Dense(timestep, activation='sigmoid'))
+            model.compile(loss='binary_crossentropy', optimizer='adam',metrics = ['accuracy'])
+            print(model.summary())
+            model.fit(X_train[indx], y_train[indx],epochs=5000,batch_size =1000,shuffle = True,callbacks=[es])
+
+            y_pre = model.predict(X_train[indx])
+            y_pre = y_pre.reshape((y_pre.shape[0]*timestep,1))
+            y_pre[y_pre>=0.5]=1
+            y_pre[y_pre<0.5]=0
+            #y_pre = np.argmax(y_pre,axis=1)
+            y_true = y_train[indx].reshape((y_train[indx].shape[0]*timestep,1))
+            #y_true = np.argmax(y_true,axis=1)
+
+            y_pre_test = model.predict(X_test)
+            y_pre_test = y_pre_test.reshape((y_pre_test.shape[0] * timestep, 1))
+            y_pre_test[y_pre_test>=0.5]=1
+            y_pre_test[y_pre_test<0.5]=0
+            #y_pre_test = np.argmax(y_pre_test, axis=1)
+            y_test_true = y_test.reshape((y_test.shape[0]*timestep,1))
+            #y_test_true = np.argmax(y_test_true,axis=1)
+
+            ptrain, rtrain, ftrain = evaluate(y_pre, y_true)
+            ptest, rtest, ftest = evaluate(y_pre_test, y_test_true)
+
+            atrain = model.evaluate(X_train[indx],y_train[indx],steps=1)
+            atest = model.evaluate(X_test,y_test,steps=1)
+            acc_train = atrain[1]
+            acc_test = atest[1]
+            loss_train = atrain[0]
+            loss_test = atest[0]
+            print(ptrain, ',', rtrain, ',', ftrain, ',', acc_train, ',', loss_train, file=open(train, "a"))
+            print(ptest, ',', rtest, ',', ftest, ',', acc_test, ',', loss_test, file=open(test, "a"))
+            if sample_size==num_seq:
+                labels = np.zeros((2, y_pre_test.shape[0]))
+                labels[0, :] = np.transpose(y_test_true)
+                labels[1, :] = np.transpose(y_pre_test)
+                f = open(indices_test,'a')
+                np.savetxt(f,labels,delimiter=',')
+                f.close()
+
+
+    print("Done")
+def Performance_evaluation(onlinecsv,output):
+    exter_eth = int('000c29d7eaec',16)
+    label_index = 14
+    data = pd.read_csv(onlinecsv)
+    data = np.array(data)
+    y_true = data.copy()
+    y_true[:,label_index] = 0
+    start_index = np.argwhere(data[:,7]==52210)
+    end_index = np.argwhere(data[:,7]==52211)
+    start_index = np.concatenate(start_index)
+    end_index = np.concatenate(end_index)
+    Flag_Diff = len(start_index)-len(end_index)
+    exter_index = np.argwhere(data[:,12:label_index]==exter_eth)
+    mtim_index = np.argwhere(data[exter_index[:,0],0:2]==297913)
+    mtim_index = exter_index[mtim_index[:,0],0]
+    #mtim_index = np.concatenate(mtim_index)
+    y_true[mtim_index,label_index] = 1
+    if Flag_Diff==0:
+        for i in range(len(start_index)):
+            A_index = np.argwhere(data[start_index[i]:end_index[i],0:2]==5884431)+start_index[i]
+            y_true[A_index[:,0],label_index]=1
+    elif Flag_Diff == 1:
+        for i in range(len(start_index)-1):
+            A_index = np.argwhere(data[start_index[i]:end_index[i],0:2]==5884431)+start_index[i]
+            y_true[A_index[:,0],label_index]=1
+        A_index = np.argwhere(data[start_index[-1]:len(data),0:2]==5884431)+start_index[i]
+        y_true[A_index[:,0],label_index]=1
+    else:
+         raise ValueError('Difference between start and end flag is greater than one.')
+    flags=np.append(start_index,end_index)
+    y_true = np.delete(y_true,flags,axis = 0)
+    data = np.delete(data,flags,axis=0)
+    print(classreport(y_true[:,label_index],data[:,label_index]))
+    np.savetxt(output,y_true,delimiter=',')
+
+def Performance_evaluation_multilabel(onlinecsv,output):
+    exter_eth = int('000c29d7eaec',16)
+    label_index = 14
+    data = pd.read_csv(onlinecsv)
+    data = np.array(data)
+    y_true = data.copy()
+    y_true[:,label_index] = 0
+    start_index = np.argwhere(data[:,7]==52210)
+    end_index = np.argwhere(data[:,7]==52211)
+    start_index = np.concatenate(start_index)
+    end_index = np.concatenate(end_index)
+    Flag_Diff = len(start_index)-len(end_index)
+    exter_index = np.argwhere(data[:,12:label_index]==exter_eth)
+    mtim_index = np.argwhere(data[exter_index[:,0],0:2]==297913)
+    mtim_index = exter_index[mtim_index[:,0],0]
+    #mtim_index = np.concatenate(mtim_index)
+    y_true[mtim_index,label_index] = 2
+    if Flag_Diff==0:
+        for i in range(len(start_index)):
+            A_index = np.argwhere(data[start_index[i]:end_index[i],0:2]==5884431)+start_index[i]
+            y_true[A_index[:,0],label_index]=1
+    elif Flag_Diff == 1:
+        for i in range(len(start_index)-1):
+            A_index = np.argwhere(data[start_index[i]:end_index[i],0:2]==5884431)+start_index[i]
+            y_true[A_index[:,0],label_index]=1
+        A_index = np.argwhere(data[start_index[-1]:len(data),0:2]==5884431)+start_index[i]
+        y_true[A_index[:,0],label_index]=1
+    else:
+         raise ValueError('Difference between start and end flag is greater than one.')
+    flags=np.append(start_index,end_index)
+    y_true = np.delete(y_true,flags,axis = 0)
+    data = np.delete(data,flags,axis=0)
+    print(classreport(y_true[:,label_index],data[:,label_index]))
+    np.savetxt(output,y_true,delimiter=',')
+
+
+#data_process('D:\\dos_pcap\\Dec2_4.csv','D:\\dos_pcap\\Dec2_4_output.csv')
+#Performance_evaluation('mtim.csv','Label_Mtim.csv')
+RNN('pcap file/label_AN_3.csv')
+#RNN('pcap file/label_mitm.csv')
+
+#Performance_evaluation_multilabel('mtim.csv','Mlabel_Mtim.csv')
---
+++ log.py
@@ -0,0 +1,194 @@
+import pandas as pd
+import numpy as np
+import keras
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
+import matplotlib.pyplot as plt
+from keras.callbacks import EarlyStopping
+from keras.utils import to_categorical
+from sklearn import preprocessing
+from keras import initializers,optimizers,losses
+import random
+import time
+import os
+def evaluate(y_pred, y_test):
+    pre = precision_score(y_true=y_test,y_pred=y_pred)
+    rec = recall_score(y_true=y_test,y_pred=y_pred)
+    f1 = f1_score(y_true=y_test,y_pred=y_pred)
+    return pre, rec, f1
+
+class LossHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.val_losses = []
+
+    def on_batch_end(self,batch,logs={}):
+        self.losses.append(logs.get('loss'))
+        self.val_loss = self.model.evaluate(self.validation_data[0],self.validation_data[1])
+        self.val_losses.append(self.val_loss[0])
+
+def pre_split(input_data, Label,test_size):
+    X = input_data[Label]
+    X = np.array(X)
+    X = preprocessing.scale(X)
+    print(X.shape)
+    num_packets = X.shape[0]
+    y = input_data['Alarm']
+    y = np.array(y,dtype=int)
+    #y = to_categorical(y)
+    test_indx =np.array(random.sample(range(num_packets),int(test_size*num_packets)))
+    #np.savetxt("ori_label.csv",np.argmax(y[test_indx],axis=1))
+    train_indx = np.setdiff1d(np.array(range(num_packets)),test_indx)
+    X_train = X[train_indx]
+    y_train = y[train_indx]
+    X_test = X[test_indx]
+    y_test = y[test_indx]
+    return X_train, y_train, X_test,y_test,train_indx,test_indx
+
+def logistic(csvname):
+    data = pd.read_csv(csvname)
+    headers = ['SourceIp','DestIp','SourcePort','destPort','Seq_num','Trans_Id','funcCode','Refno','Register_data','Exeption_Code','Time_Stamp','Relative_Time','eth_src','eth_dst','Alarm']
+    data.columns = headers
+    del(data['eth_src'])
+    del(data['eth_dst'])
+    X_Label = [i for i in data.columns.tolist() if i not in 'Alarm']
+    X, y = pre_split(data, X_Label)
+    Train_Error = []
+    Test_Error = []
+    es = EarlyStopping(monitor='loss',min_delta = 0.00005, patience = 5)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    num_seq = X_train.shape[0]
+    #num_sample = [10, 100, 500, 1000, 5000, 10000, 30000, 50000, 80000, 100000, 150000, 200000, num_seq]
+    num_sample = [num_seq]
+    for sample_size in num_sample:
+        #print(sample_size,file=open("10fold/Log_10_fold_train.txt","a"))
+        #print("Precision",',',"Recall",',',"F1",',',"Accuray",',',"loss",file=open("10fold/Log_10_fold_train.txt","a"))
+        #print(sample_size,file=open("10fold/Log_10_fold_test.txt","a"))
+        #print("Precision",',',"Recall",',',"F1",',',"Accuray",',',"loss",file=open("10fold/Log_10_fold_test.txt","a"))
+        
+        for kfold in range(1):
+            model = Sequential()
+            model.reset_states()
+            model.add(Dense(y.shape[1],input_dim= 12,activation='sigmoid'))
+            model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+            model.fit(X_train[0:sample_size, :], y_train[0:sample_size], batch_size = 256, epochs=10000, shuffle=True,callbacks=[es])
+            y_pre_train = model.predict_classes(X_train[0:sample_size, :])
+            y_pre_test = model.predict_classes(X_test)
+            y_pre_train = y_pre_train.reshape(sample_size)
+            ftrain = f1_score(y_true=y_train[0:sample_size],y_pred=y_pre_train)
+            ptrain = precision_score(y_true=y_train[0:sample_size],y_pred=y_pre_train)
+            rtrain = recall_score(y_true=y_train[0:sample_size],y_pred=y_pre_train)
+            
+            ftest = f1_score(y_true=y_test,y_pred=y_pre_test)
+            ptest = precision_score(y_true=y_test,y_pred=y_pre_test)
+            rtest = recall_score(y_true=y_test,y_pred=y_pre_test)
+
+            atrain = model.evaluate(X_train[0:sample_size, :], y_train[0:sample_size])
+            atest = model.evaluate(X_test,y_test)
+            acc_train = atrain[1]
+            acc_test = atest[1]
+            loss_train = atrain[0]
+            loss_test = atest[0]
+            #print(ptrain,',',rtrain,',',ftrain,',',acc_train,',',loss_train,file=open("10fold/Log_10_fold_train.txt","a"))
+            #print(ptest,',',rtest,',',ftest,',',acc_test,',',loss_test,file=open("10fold/Log_10_fold_test.txt","a"))
+            
+
+
+
+def logistic_nondos(csvname):
+    data = pd.read_csv(csvname)
+    headers = ['SourceIp','DestIp','SourcePort','destPort','Seq_num','Trans_Id','funcCode','Refno','Register_data','Exeption_Code','Time_Stamp','Relative_Time','HH','LL','H','L','speed','tank1','tank2','Alarm']
+    tm = time.ctime()+'/'
+    path = 'FNN_results/'
+    os.mkdir(path+tm)
+    os.mkdir(path+tm+'model/')
+    train = path+tm+"train.csv"
+    test = path+tm+"test.csv"
+    #label_train_pre=path+tm+"label_train_pre.csv"
+    #label_train_act = path+tm+"label_train_act.csv"
+    #label_test_pre = path + tm+"label_test_pre.csv"
+    #label_test_act = path+tm+"label_test_act.csv"
+    indices_test = path+tm+"indices_test.txt"
+    #indices_train = path +tm+ "indices_train.csv"
+    model_path = path+tm+'model/'
+    data.columns = headers
+    data = data.dropna()
+    X_Label = [i for i in data.columns.tolist() if i not in 'Alarm']
+    X_train, y_train,X_test,y_test, train_indx,test_indx = pre_split(data, X_Label,0.3)
+    
+    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=193)
+
+    num_seq = X_train.shape[0]
+    num_sample =[22,50,100,1000,3000,6000,10000,30000,70000,100000,150000,170000,num_seq]
+    #num_sample = [num_seq]
+    for sample_size in num_sample:
+        es = EarlyStopping(monitor='loss', min_delta=1e-6,patience = 35)
+        print(sample_size,file=open(train,"a"))
+        print("Precision",',',"Recall",',',"F1",',',"Accuray",',',"loss",file=open(train,"a"))
+        print(sample_size,file=open(test,"a"))
+        print("Precision",',',"Recall",',',"F1",',',"Accuray",',',"loss",file=open(test,"a"))
+        #print(sample_size,file=open(label_train,"a"))
+        #print(sample_size,file=open(label_test,"a"))
+
+        for kfold in range(10):
+            print(sample_size)
+             
+            model = Sequential()
+            indx =np.array(random.sample(range(X_train.shape[0]),sample_size))
+            print(X_train[indx].shape)
+            model.reset_states()
+            model.add(Dense(100,activation ='relu'))
+            #model.add(Dense(100,activation='relu'))
+            #model.add(Dense(100,activation='relu'))
+            #model.add(Dense(50,activation='relu'))
+            #model.add(Dense(20,activation='relu'))
+            #model.add(Dense(100,kernel_initializer=initializers.RandomNormal(stddev=0.1),activation ='relu'))
+
+            model.add(Dense(1,activation='sigmoid'))
+            model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+            model.fit(X_train[indx], y_train[indx], batch_size = 1000,shuffle=True,epochs=10000,callbacks=[es])
+            model.save(model_path+str(sample_size)+'_'+str(kfold)+".h5")
+            y_pre_train = model.predict_classes(X_train[indx])
+            y_pre_test = model.predict_classes(X_test)
+            #print(X_test)
+            #y_true_train = np.argmax(y_train[indx],axis=1) 
+            #y_true_test = np.argmax(y_test,axis=1)
+            ptrain,rtrain,ftrain = evaluate(y_pre_train,y_train[indx])
+            ptest,rtest,ftest = evaluate(y_pre_test,y_test)
+
+            atrain = model.evaluate(X_train[indx], y_train[indx],steps=1)
+            atest = model.evaluate(X_test,y_test,steps=1)
+            acc_train = atrain[1]
+            acc_test = atest[1]
+            loss_train = atrain[0]
+            loss_test = atest[0]
+            print(ptest,',',rtest,',',ftest,',',acc_test,',',loss_test,file=open(test,"a"))
+            print(ptrain,',',rtrain,',',ftrain,',',acc_train,',',loss_train,file=open(train,"a"))
+
+            '''print("Train_label",file=open(label_train_act,"a"))
+            f = open(label_train_act,'a')
+            np.savetxt(f,y_true_train,delimiter=',')
+            f.close()
+            print("Train_predictions_Label",file=open(label_train_pre,"a"))
+            f = open(label_train_pre,'a')
+            np.savetxt(f,y_pre_train,delimiter=',')
+            f.close()'''
+            #true label first, predictions second
+            if sample_size == num_seq:
+
+                labels = np.zeros((3,y_test.shape[0]))
+                labels[0,:]= test_indx+2
+                labels[1,:]= y_test
+                labels[2,:]= np.transpose(y_pre_test)
+                f = open(indices_test,'a')
+                np.savetxt(f,labels,delimiter=',')
+                f.close()
+
+
+#logistic('Label_Jan27b.csv')
+#NN('Label_Jan27b.csv')
+#logistic('Mlabel_Mtim.csv')
+#logistic_nondos('pcap file/label_AN_3.csv')
+logistic_nondos('pcap file/label_AN_3.csv')
